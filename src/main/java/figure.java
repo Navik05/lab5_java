d0307989d447a80a81980a3c28ff98b6b2cfdfb1
naviko <naviko05@mail.ru>
2023-12-19 12:44:40 +0700

    Initial commit

---
+++ src/main/java/figure.java
@@ -0,0 +1,14 @@
+public class figure {
+    protected double area;
+    protected double perimeter;
+    protected static int type;
+    public double getArea() {
+        return area;
+    }
+    public double getPerimeter() {
+        return perimeter;
+    }
+    public static void setType(int e) {
+        type = e;
+    }
+}
